d04eec3140c1f2461379ae0d323b6f416d39b565
Engin Karatas <enginkaratas99@gmail.com>
2024-09-08 19:16:44 +0300

    nodejs now sent request dbus

---
+++ server.js
@@ -0,0 +1,48 @@
+const http = require("node:http");
+
+const hostname = "127.0.0.1";
+const port = 3000;
+
+const DBus = require("dbus"); // Adjust the path as necessary
+
+const bus = DBus.getBus("session"); // Use 'system' if your server is on the system bus
+
+const serviceName = "in.softprayog.add_server";
+const objectPath = "/in/softprayog/adder";
+const interfaceName = "in.softprayog.dbus_example";
+
+const url = require("url");
+
+const server = http.createServer((req, res) => {
+  const reqUrl = url.parse(req.url, true); // GET endpoint
+  if (reqUrl.pathname == "/users" && req.method === "GET") {
+    console.log("Request type: " + req.method + " Endpoint: " + req.url);
+
+    bus.getInterface(serviceName, objectPath, interfaceName, (err, iface) => {
+      if (err) {
+        console.error("Error getting interface:", err);
+        return;
+      }
+
+      if (!iface.add_numbers) {
+        console.error("Method add_numbers not found on interface");
+        return;
+      }
+
+      // Call the add_numbers method
+      iface.add_numbers("44 33", { timeout: 1000 }, (err, result) => {
+        if (err) {
+          console.error("Error calling method:", err);
+          return;
+        }
+
+        console.log("Result from server:", result);
+        res.statusCode = 200;
+        res.setHeader("content-Type", "Application/json");
+        res.end(JSON.stringify(result));
+      });
+    });
+  }
+});
+
+server.listen(port, () => console.log(`server listening on port: ${port}`));
